b8a1b9b954e1158f817cb526b7df5012e773dde4
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2020-05-14 22:41:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,41 @@ import argparse
 import logging
 import os
 import time
+from tqdm import tqdm
+import random
+import torch
+from torch.autograd import Variable
 
 from data import processPoetry,build_vocabulary,Vocabulary,Poetry
 from model import LSTMPoetry
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+def train(model,dataset,criterion,optimizer,args,device):
+    sampler = torch.utils.data.sampler.RandomSampler(dataset)
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size,
+        sampler=sampler,
+        num_workers=args.data_workers,
+        pin_memory=args.cuda,
+    )
+    model.train()
+    optimizer.zero_grad()
+    for epoch in range(args.epoches):
+        total_loss = 0.0
+        for item in tqdm(data_loader, desc="Training epoch " + str(epoch + 1) + ''):
+            (in_title, in_poetry), (tar_title, tar_poetry) = item
+            in_title,in_poetry = Variable(in_title).to(device),Variable(in_poetry).to(device)
+            tar_title, tar_poetry = Variable(tar_title).to(device),Variable(tar_poetry).to(device)
+            titoutput, output = model.forward(in_title,in_poetry)
+            loss1 = criterion.forward(titoutput.transpose(1, 2), tar_title)
+            Length = len(output)
+            loss2 = criterion.forward(output.transpose(1, 2), tar_poetry.reshape(Length, -1))
+            loss = loss1+loss2
+            loss.backward()
+            total_loss += loss.item()
+            optimizer.step()
+            optimizer.zero_grad()
+        print("loss is {},epoch is {}".format(total_loss/len(data_loader),epoch+1))
+    torch.save(model,)
 def main():
     parser = argparse.ArgumentParser(
         description='PyTorch RNNs for Poetry Generation')
@@ -17,12 +48,19 @@ def main():
     parser.add_argument('--wordnum',default=5,help='The number of poetry words in the sentences.',type=int)
     parser.add_argument('--sentnum', default=4, help='The number of poetry sentences.', type=int)
     parser.add_argument('--max-len', default=20, help='The number of poetry titles.', type=int)
-    parser.add_argument('--batch-size', default=15, help='The number of batch-size.', type=int)
     parser.add_argument('--embedding-dim', default=300, help='The dimension of embedding .', type=int)
     parser.add_argument('--hidden-dim', default=150, help='The dimension of hidden .', type=int)
-    parser.add_argument('--hidden-dim', default=150, help='The dimension of hidden .', type=int)
     parser.add_argument('--num_layers', default=2, help='The rnn layers.', type=int)
+    parser.add_argument('--batch-size', default=30, help='The batch-size of the dataset.', type=int)
+    parser.add_argument('--data-workers', type=int, default=5,help='Number of subprocesses for data loading')
+    parser.add_argument('--epoches', default=50, help='The batch-size of the dataset.', type=int)
     parser.add_argument('--bidirectional', action='store_true', help='Whether using bidirectional RNNs')
+    parser.add_argument('--lr', default=0.001, type=float, metavar='LR', help='initial learning rate')
+    parser.add_argument('--seed', default=123, type=int, help='random seed (default: 123)')
+    cuda_parser = parser.add_mutually_exclusive_group(required=False)
+    cuda_parser.add_argument('--cuda', dest='cuda', action='store_true')
+    cuda_parser.add_argument('--no-cuda', dest='cuda', action='store_false')
+    parser.set_defaults(cuda=True)
     args = parser.parse_args()
     # preparing log
     # logging defination
@@ -39,6 +77,13 @@ def main():
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     logger.info(args)
+    args.cuda = args.cuda and torch.cuda.is_available()
+    device = torch.device("cuda:0" if args.cuda else "cpu")
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+        torch.backends.cudnn.benchmark = True
     assert (args.rawdir is not None)
     # preparing dataset
     poetry_path = os.path.join(args.datadir, "poet.%s._%d_%d.json" % (args.tag, args.sentnum, args.wordnum))
@@ -62,8 +107,15 @@ def main():
     PoetryDataSet = Poetry(VocabDataSet,args.max_len,poetry_path)
 
     # preparing model
-    model = LSTMPoetry(vocab_size=len(VocabDataSet),embedding_dim=args.embedding_dim,hidden_dim=args.hidden_dim,
-                       sents_len=args.sentnum,num_layers=args.num_layers)
 
+    model = LSTMPoetry(vocab_size=len(VocabDataSet),embedding_dim=args.embedding_dim,hidden_dim=args.hidden_dim,
+                       sents_len=args.sentnum,num_layers=args.num_layers,name = model_name)
+    criterion = torch.nn.CrossEntropyLoss()
+    model.to(device), criterion.to(device)
+    optimizer = torch.optim.Adam(model.parameters())
+    # training process
+    logger.info("Begin training model!")
+    train(model,PoetryDataSet,criterion,optimizer,args,device)
+    logger.info("End training model!")
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import numpy as np
 class LSTMSentence(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim,num_layers=2,bidirectional = False):
         super(LSTMSentence, self).__init__()
@@ -11,34 +11,21 @@ class LSTMSentence(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.bidirectional = bidirectional
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers,bidirectional = bidirectional,batch_first=True)
         self.linear = nn.Linear(self.hidden_dim, vocab_size)
-        self.softmax = nn.Softmax(dim=2)
-    def forward(self, input, hidden=None):
-        batch_size = input.size(0)
+        self.softmax = nn.LogSoftmax(dim=2)
+    def forward(self, embput, hidden=None):
+        batch_size = embput.size(0)
         if hidden is None:
-            h_0,c_0 = self.init_hid(batch_size)
+            h_0,c_0 = self.init_hid(embput,batch_size)
         else:
             h_0, c_0 = hidden
-        # size: (seq_len,batch_size,embeding_dim)
-        embeds = self.embeddings(input)
         # output size: (seq_len,batch_size,hidden_dim)
-        output, hidden = self.lstm(embeds, (h_0, c_0))
+        output, hidden = self.lstm(embput, (h_0, c_0))
         hid = self.linear(output)
         pred = self.softmax(hid)
         return pred,hidden
-    def predict(self,words:torch.LongTensor):
-        assert words.dim()==1
-        h_0,c_0 = self.init_hid(1)
-        # size: (seq_len,batch_size,embeding_dim)
-        embeds = self.embeddings(words)
-        # output size: (seq_len,batch_size,hidden_dim)
-        output, hidden = self.lstm(embeds, (h_0, c_0))
-        hid = self.linear(output)
-        pred = torch.argmax(self.softmax(hid),dim=2)
-        return pred
-    def init_hid(self,batch_size):
+    def init_hid(self,input,batch_size):
         num_directions = 2 if self.bidirectional else 1
         h_0 = input.data.new(num_directions * self.num_layers, batch_size, self.hidden_dim).fill_(0).float()
         c_0 = input.data.new(num_directions * self.num_layers, batch_size, self.hidden_dim).fill_(0).float()
@@ -46,7 +33,7 @@ class LSTMSentence(nn.Module):
 #表示七言绝句诗词还是五言绝句诗词还是七言律诗还是五言律诗
 #        律诗一般是8句，绝句是4句，标题的长度没有限制
 class LSTMPoetry(nn.Module):
-    def __init__(self,vocab_size, embedding_dim, hidden_dim,sents_len=8,num_layers=2,bidirectional = False):
+    def __init__(self,vocab_size, embedding_dim, hidden_dim,sents_len=8,num_layers=2,bidirectional = False,name = None):
         super(LSTMPoetry, self).__init__()
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
@@ -55,27 +42,72 @@ class LSTMPoetry(nn.Module):
         self.bidirectional = bidirectional
         self.sents_len = sents_len
         self.layers = []
+        self.name = name
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
         for k in range(sents_len):
-            self.layers.append(LSTMSentence(vocab_size, embedding_dim, hidden_dim, num_layers, bidirectional))
-        self.title_rnn = nn.LSTM(hidden_dim,hidden_dim,num_layers=num_layers,bidirectional=bidirectional)
-        self.title_linear = nn.Linear(hidden_dim,vocab_size,bias=False)
+            fc = LSTMSentence(vocab_size, embedding_dim, hidden_dim, num_layers, bidirectional)
+            setattr(self, "fc%d" % k, fc)
+            self.layers.append(fc)
+        self.title_rnn = nn.LSTM(embedding_dim,hidden_dim,num_layers=num_layers,bidirectional=bidirectional,batch_first=True)
+        self.title_linear = nn.Linear(hidden_dim*sents_len,vocab_size,bias=False)
     def forward(self,title,sents):
-        batch_size =title.size(0)
+        '''
+        :param title:(batch_size*seq_len)
+        :param sents:(batch_size*sent_nums*seq_len)
+        :return:
+        '''
+        batch_size,sent_len,seq_len = sents.size()
+        assert (sent_len == self.sents_len)
         output = []
-        h,c = self.layers[0].init_hid(batch_size)#(num_directions*num_layers) batch * vocab_size
-        out_title = title
-        for k in range(self.sents_len-1):
-            out,(h,c) = self.layers[k].forward(sents[k+1],(h,c)) #batch * seq_len * vocab_size
-            out_title,_ = self.title_rnn(out_title,(h,c))
+        h,c = self.layers[0].init_hid(sents,batch_size) #(num_directions*num_layers) batch * vocab_size
+        titoutput = []
+        for k in range(sent_len):
+            embput = self.embeddings(sents[:,k,:])
+            out,(h,c) = self.layers[k].forward(embput,(h,c)) #batch * seq_len * vocab_size
+            titemb = self.embeddings(title)
+            out_title,_ = self.title_rnn(titemb,(h,c))
             out_title = F.gelu(out_title)
             output.append(out)
-        # 首尾呼应
-        out, (h, c) = self.layers[self.sents_len-1].forward(sents[0], (h, c))  # batch * seq_len * vocab_size
-        out_title, _ = self.title_rnn(out_title, (h, c))
-        out_title = F.gelu(out_title)
-        output.append(out)
-
+            titoutput.append(out_title)
         output = torch.cat(output,dim=1)
-        self.title_linear(out_title)
-        out_title = F.softmax(out_title,dim=2)
-        return out_title,output
+        titoutput = torch.cat(titoutput,dim = 2)
+        titoutput = self.title_linear(titoutput)
+        titoutput = F.log_softmax(titoutput,dim=2)
+        return titoutput,output
+    def predict(self,words:set,max_len:int,wordnum:int):
+        '''
+        由一些输入的词语进行描写诗词中的语境来刻画诗词意境
+        生成的方法如下：
+        1.设输入的词语集合为Set,初始化h,c.随机从Set中抽取若干个词语(可以重复)组成生成第一句话的引导语句pre_sent1
+        2.先生成第一句话pre_sent1,(h,c)->sent1,(h,c),随机抽取sent1中的若干个词语组成题目输入向量值，
+        将(h,c)输入到题目生成器中，得到题目输出向量tit_v1
+        3.将sent1中的词语加入到Set当中,随机从Set中抽取若干个词语(可以重复)组成生成第一句话的引导语句pre_sent2
+        4.生成第二句话pre_sent2,(h,c)->sent2,(h,c),随机抽取sent2中的若干个词语组成题目输入向量值，
+        将(h,c)输入到题目生成器中，得到题目输出向量tit_v2
+        ...
+        n.生成第n句话,pre_sent(n-1),(h,c)->sentn,(h,c),随机抽取sentn中的若干个词语组成题目输入向量值，
+        将(h,c)输入到题目生成器中，得到题目输出向量tit_vn
+        n+1.合并(tit_v1,tit_v2,...,tit_vn)，并输入到线性变换空间中，然后生成题目:
+        注意在每一次输入题目预生词中词语的个数不超过题目的长度，其中的空位置用<BLK>标注，结尾用<EOS>标注。
+        每一次输入的词语开头处必须是用<SOS>标注。
+        :param words: 输入预生词的集合Set
+        :param max_len:输入的题目长度
+        :return: 输出诗句index
+        '''
+        words = list(words)
+        pre_sent = []
+        for k in range(wordnum):
+            pre_sent.append(words[np.random.randint(0, len(words))])
+        pre_sent = torch.LongTensor(pre_sent).unsqueeze(0)
+        h, c = self.layers[0].init_hid(pre_sent,1)
+        poetry = []
+        for st_num in range(len(self.layers)):
+            embput = self.embeddings(pre_sent)
+            out, (h, c) = self.layers[st_num].forward(embput, (h, c))
+            out = torch.argmax(out.squeeze(),dim=0)
+            poetry.append(out.numpy().tolist())
+            # 生成题目
+            words = list(words)
+            pre_sent = []
+            for k in range(wordnum):
+                pre_sent.append(words[np.random.randint(0,len(words))])
